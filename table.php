596ac9e5c6d81623dc7620ad751fe39799997680
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-09 14:01:35 +0200

    created style_table.css & finished work in table.php

--- table.php
+++ table.php
@@ -5,9 +5,74 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="style_table.css">
+    <title>Firs Table</title>
 </head>
 <body>
+<center>
+<table border="2" >
+    <!--1-->
+    <tr>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
+        <td>15</td>
+        <td>16</td>
+        <td colspan="2">17335555</td>
+        <td>18</td>
+        <td>19</td>
+    </tr>
+    <!--2-->
+    <tr>
+        <td rowspan="2" colspan="2">21</td>
+        <td>22</td>
+        <td>23</td>
+        <td>24</td>
+        <td>25</td>
+        <td colspan="2">28</td>
+        <td>29</td>
+        <td>210</td>
+    </tr>
+    <!--3-->
+    <tr>
+        <td>31</td>
+        <td>123</td>
+        <td>134</td>
+        <td>134</td>
+        <td colspan="2">134</td>
+        <td colspan="2">123</td>
+
+    </tr>
+    <!--4-->
+    <tr>
+        <td>41</td>
+        <td></td>
+        <td colspan="3" rowspan="2"></td>
+        <td>123</td>
+        <td colspan="2">134</td>
+        <td>134</td>
+        <td>123</td>
+    </tr>
+    <!--5-->
+    <tr>
+        <td>51</td>
+        <td>65</td>
+        <td rowspan="2">65</td>
+        <td colspan="2">34</td>
+        <td colspan="2">34</td>
+    </tr>
+    <!--6-->
+    <tr>
+        <td colspan="2">61</td>
+        <td></td>
+        <td></td>
+        <td>123</td>
+        <td colspan="2">134</td>
+        <td colspan="2">123</td>
+    </tr>
+</table>
+</center>
 
 </body>
 </html>
