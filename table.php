2ac275830355680556427f6e9f0cb70836e5887e
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-12 12:35:30 +0200

    cleaned cells

--- table.php
+++ table.php
@@ -9,66 +9,65 @@
     <title>Firs Table</title>
 </head>
 <body>
-<table border="2" align="center" width=550>
+<table border="1" align="center" width=550>
     <!--1-->
     <tr>
-        <td>11</td>
-        <td>12</td>
-        <td>13</td>
-        <td>14</td>
-        <td>15</td>
-        <td>16</td>
-        <td colspan="2">15</td>
-        <td>18</td>
-        <td>19</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td></td>
     </tr>
     <!--2-->
     <tr>
-        <td rowspan="2" colspan="2">21</td>
-        <td>22</td>
-        <td>23</td>
-        <td>24</td>
-        <td>25</td>
-        <td colspan="2">28</td>
-        <td>29</td>
-        <td>210</td>
+        <td rowspan="2" colspan="2"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td></td>
     </tr>
     <!--3-->
     <tr>
-        <td>31</td>
-        <td>123</td>
-        <td>134</td>
-        <td>134</td>
-        <td colspan="2">134</td>
-        <td colspan="2">123</td>
-
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
     </tr>
     <!--4-->
     <tr>
-        <td>41</td>
-        <td>32</td>
-        <td colspan="3" rowspan="2">12</td>
-        <td>123</td>
-        <td colspan="2">134</td>
-        <td>134</td>
-        <td>123</td>
+        <td></td>
+        <td></td>
+        <td colspan="3" rowspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td></td>
     </tr>
     <!--5-->
     <tr>
-        <td>51</td>
-        <td>65</td>
-        <td rowspan="2">65</td>
-        <td colspan="2">34</td>
-        <td colspan="2">34</td>
+        <td></td>
+        <td></td>
+        <td rowspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
     </tr>
     <!--6-->
     <tr>
-        <td colspan="2">61</td>
+        <td colspan="2"></td>
         <td></td>
         <td></td>
-        <td>123</td>
-        <td colspan="2">134</td>
-        <td colspan="2">123</td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
     </tr>
 </table>
 </body>
