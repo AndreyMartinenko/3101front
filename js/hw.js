17f0436cc7401f782938e046b48a20c73a4e8c9f
AndreyMartinenko <Andremartinenko@gmail.com>
2018-03-05 13:30:54 +0200

    Add files via upload
    js HW

---
+++ js/hw.js
@@ -0,0 +1,33 @@
+
+
+    alert('hello friend!');
+    //second task
+
+    var firstName = 'Alex';
+        lastName = firstName;
+
+    alert("it's second task - "+lastName);
+
+    //third task
+    alert('third task - look to the console');
+    var name = 'John';
+    var secondName = 'Smith';
+    var age = 65;
+    var knownDegree =  'medium';
+    var hobby = 'chess';
+    console.log(name,secondName, age, knownDegree, hobby);
+
+
+    //fourth task
+    alert("fourth task: "+" undefinite x; "+" d= "+2+
+            "; c=" +4+ "; d=" +4+ ": a= " +2+ "; b= "+2);
+
+   // alert("d="+2+ "; c="+4+"; d="+4+ ": a="+2+"; b= "+2);
+
+
+
+var a,b,c;
+a = Number(prompt('Fifth task - enter first number', a));
+b = Number(prompt('Fifth task - enter second number', b));
+c = a+b;
+ alert (a+"+"+b+"="+c);
